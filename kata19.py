c8e4a8fbc39e0ff313fdacd3225d1e2a83a0a6b4
Pietro Pacanaro <pietro.macho12@gmail.com>
2024-07-24 19:54:36 -0300

    deleting test comment

--- kata19.py
+++ kata19.py
@@ -3,8 +3,6 @@
 # Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
 # Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
 
-#teste
-
 def open_or_senior(data: list):
     result = []
     for pair in data:
